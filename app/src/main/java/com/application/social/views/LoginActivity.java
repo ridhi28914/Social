655e67811e045d481851762d37a014dfe7fac5d1
Vj94 <nainypal@gmail.com>
2017-05-21 23:43:13 +0530

    Splash screen

--- app/src/main/java/com/application/social/views/LoginActivity.java
+++ app/src/main/java/com/application/social/views/LoginActivity.java
@@ -1,4 +1,10 @@
 package com.application.social.views;
+package info.androidhive.androidsplashscreentimer;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.Handler;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,6 +15,7 @@ import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000;
     SharedPreferences sharedPreference;
     SharedPreferences.Editor editor;
     @Override
@@ -18,15 +25,31 @@ public class LoginActivity extends AppCompatActivity {
         sharedPreference = getApplicationContext().getSharedPreferences("TokenPreference", 0);
         editor = sharedPreference.edit();
         String accessToken=sharedPreference.getString("access_token",null);
-        if (accessToken!=null) {
-            Intent intent = new Intent(this, Home.class);
-            startActivity(intent);
-            finish();
-        }
-        else{
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                if (accessToken!=null) {
+                    Intent intent = new Intent(this, Home.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, SPLASH_TIME_OUT);
+
+
     }
 }
