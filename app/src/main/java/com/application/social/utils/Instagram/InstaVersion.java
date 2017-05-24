80dfa84edce9bd3637f6706c1995432d0926d204
RavleenVirdi <RavleenVirdi@github.com>
2017-05-25 01:24:09 +0530

    insta feed displayed

--- app/src/main/java/com/application/social/utils/Instagram/InstaVersion.java
+++ app/src/main/java/com/application/social/utils/Instagram/InstaVersion.java
@@ -1,20 +1,59 @@
 package com.application.social.utils.Instagram;
 
+import java.util.ArrayList;
+
 /**
  * Created by Ridhi on 5/23/2017.
  */
 
 public class InstaVersion {
-//    private String version_name;
-    private String image_url;
 
-//    public String getVersion_name() {
-//        return version_name;
-//    }
-//
-//    public void setVersion_name(String version_name) {
-//        this.version_name = version_name;
-//    }
+    private String image_url;
+    private String user_name;
+    private String profile_picture;
+    private String caption;
+    private String likes;
+    private String comments;
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public String getProfile_picture() {
+        return profile_picture;
+    }
+
+    public void setProfile_picture(String profile_picture) {
+        this.profile_picture = profile_picture;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
 
     public String getImage_url() {
         return image_url;
